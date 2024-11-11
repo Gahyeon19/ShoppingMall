9f88064700e62c75f1d12438c45e0b032ec45760
Gahyeon19 <wjsrkgus0119@naver.com>
2024-11-11 10:33:07 +0900

    made package

--- src/main/java/com/example/shoppingmall/dto/member/MemberCreateDto.java
+++ src/main/java/com/example/shoppingmall/dto/member/MemberCreateDto.java
@@ -1,4 +1,4 @@
-package com.example.shoppingmall.dto;
+package com.example.shoppingmall.dto.member;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
--- src/main/java/com/example/shoppingmall/dto/member/MemberInquiryDto.java
+++ src/main/java/com/example/shoppingmall/dto/member/MemberInquiryDto.java
@@ -1,4 +1,4 @@
-package com.example.shoppingmall.dto;
+package com.example.shoppingmall.dto.member;
 
 import com.example.shoppingmall.entity.Member;
 import lombok.AllArgsConstructor;
