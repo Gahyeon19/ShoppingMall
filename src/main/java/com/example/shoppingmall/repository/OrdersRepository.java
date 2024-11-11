dc9018b574fab996ef96769094c35153995888b8
Gahyeon19 <wjsrkgus0119@naver.com>
2024-11-11 10:38:03 +0900

    add method

--- src/main/java/com/example/shoppingmall/repository/OrdersRepository.java
+++ src/main/java/com/example/shoppingmall/repository/OrdersRepository.java
@@ -1,7 +1,11 @@
 package com.example.shoppingmall.repository;
 
+import com.example.shoppingmall.entity.Member;
 import com.example.shoppingmall.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OrdersRepository extends JpaRepository<Orders, Long> {
+    Optional<Orders> findByMember(Member member);
 }
--- src/main/java/com/example/shoppingmall/repository/StockRepository.java
+++ src/main/java/com/example/shoppingmall/repository/StockRepository.java
@@ -1,7 +1,12 @@
 package com.example.shoppingmall.repository;
 
+import com.example.shoppingmall.entity.Product;
 import com.example.shoppingmall.entity.Stock;
+import com.example.shoppingmall.entity.WareHouse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StockRepository extends JpaRepository<Stock, Integer> {
+    Optional<Stock> findByWareHouseAndProduct(WareHouse wareHouse, Product product);
 }
