0c2f05d5c7978ac61d261237fbd9a357ccef9d09
Gahyeon19 <wjsrkgus0119@naver.com>
2024-11-12 16:51:06 +0900

    add methods

--- src/main/java/com/example/shoppingmall/repository/OrderProductRepository.java
+++ src/main/java/com/example/shoppingmall/repository/OrderProductRepository.java
@@ -1,7 +1,11 @@
 package com.example.shoppingmall.repository;
 
 import com.example.shoppingmall.entity.OrderProduct;
+import com.example.shoppingmall.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderProductRepository extends JpaRepository<OrderProduct, Long> {
+    List<OrderProduct> findAllByOrders(Orders order);
 }
--- src/main/java/com/example/shoppingmall/repository/OrdersRepository.java
+++ src/main/java/com/example/shoppingmall/repository/OrdersRepository.java
@@ -4,8 +4,10 @@ import com.example.shoppingmall.entity.Member;
 import com.example.shoppingmall.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrdersRepository extends JpaRepository<Orders, Long> {
-    Optional<Orders> findByMember(Member member);
+    Optional<Orders> findByMember(Member member);   //불필요 - refactoring 필요
+    List<Orders> findAllByMember(Member member);
 }
